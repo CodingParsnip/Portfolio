c85eeb9205e90985339caef21c35fdb754f8275a
Claudia Parker <132288790+CodingParsnip@users.noreply.github.com>
2023-08-26 02:31:30 -0500

    Added more adjectives to Level-Generator.js

--- level-generator/Level-Generator.js
+++ level-generator/Level-Generator.js
@@ -1,9 +1,9 @@
 // BUILDING THE GENERATOR
 
-const section1 = ['Hot', 'Wet', 'Cold', 'Dry', 'Chilly', 'Spicy', 'Melting', 'Burning', 'Joyful', 'Wild', 'Goofy', 'Lonely', 'Abandoned', 'Stinky', 'Windy', 'Red', 'Blue', 'Green', 'Lovely', 'Radical', 'Yellow'];
+const section1 = ['Hot', 'Wet', 'Cold', 'Dry', 'Chilly', 'Spicy', 'Melting', 'Burning', 'Joyful', 'Wild', 'Goofy', 'Lonely', 'Abandoned', 'Stinky', 'Windy', 'Red', 'Blue', 'Green', 'Lovely', 'Radical', 'Yellow', 'Gassy', 'Gnarly', 'Haunted', 'Flooded'];
 const section2 = ['Mountain', 'Ocean', 'Valley', 'Canyon', 'Space', 'City', 'Forest', 'Highway', 'Coast', 'Beach', 'Undersea', 'Volcano', 'Geyser', 'Village'];
 const section3 = ['Racetrack', 'Zone', 'Farm', 'Circus', 'House', 'Mansion', 'Complex', 'Jailhouse', 'Campus', 'Playground', 'Cove', 'Cavern', 'Cottage', 'Fields', 'Pass', 'Run', 'Plains', 'Lab', 'Site', 'Chasm', 'Warehouse'];
-//Section 1: 21 || Section 2: 14 || Section 3: 21
+//Section 1: 25 || Section 2: 14 || Section 3: 21
 
 const randSection = (section) =>
 {
